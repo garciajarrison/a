dec4ae55e0420bd7ff507f2f75ef77d577ce8e13
Jarrison Garcia <garciajarrison@gmail.com>
2018-07-25 20:58:49 -0500

    sdsd

---
+++ Tienda/src/co/com/tienda/controller/LoginBB.java
@@ -0,0 +1,15 @@
+package co.com.tienda.controller;
+
+import java.io.Serializable;
+
+public class LoginBB implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+	
+	
+	
+	
+	
+	
+
+}
